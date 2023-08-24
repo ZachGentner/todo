605f637d6e3630ae1a6e1c9a15266a3071a6794d
Zach Gentner <zach.gentner@gmail.com>
2023-08-24 11:15:00 -0400

    formatting and search functionality

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const form = document.querySelector("form");
+//VARIABLES-QUERY SELECTORS
+const form = document.querySelector('form');
 const input = document.querySelector('#input');
 const list = document.querySelector('#list');
-const checks = document.querySelectorAll("#list input");
+const search = document.querySelector('#search');
 
-//Add events
+//EVENT HANDLERS
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     addTask();
@@ -11,19 +12,22 @@ form.addEventListener("submit", (event) => {
 
 list.addEventListener("click", (event) => {
     removeTask(event);
-    console.log(event.target)
 });
 
+search.addEventListener("input", (event) => {
+    searchList(event);
+})
 
+
+//ADD A TASK TO THE LIST
 function addTask() {
     if (input.value != "") {
         //Create a new line item element.
         let task = document.createElement("li");
 
-
         //Create and append checkbox
         let checkbox = document.createElement("button");
-        checkbox.className = "checkbox btn btn-secondary";
+        checkbox.className = "checkbox btn btn-warning";
         let icon = document.createElement("i");
         icon.className = "fa-solid fa-check";
 
@@ -32,6 +36,7 @@ function addTask() {
 
         //Create and append content
         let content = document.createElement("p")
+        content.className = "m-2";
         content.innerText = input.value;
         task.appendChild(content);
 
@@ -41,10 +46,27 @@ function addTask() {
     }
 }
 
+//REMOVE A TASK FROM THE LIST
 function removeTask(e) {
     if(e.target.classList.contains("checkbox")) {
         e.target.parentElement.remove();
+    } else if (e.target.parentElement.classList.contains("checkbox")) {
+        e.target.parentElement.parentElement.remove();
     }
 }
 
- 
+//SEARCH FUNCTIONALITY
+function searchList(e) {
+    tasks = Array.from(list.children);
+
+    tasks.forEach((task) => {
+        const taskText = task.querySelector("p").innerText.toLowerCase();
+        const searchValue = search.value.toLowerCase();
+    
+        if (searchValue === "" || taskText.includes(searchValue)) {
+            task.style.display = "flex";
+        } else {
+            task.style.display = "none";
+        }
+    });
+}
