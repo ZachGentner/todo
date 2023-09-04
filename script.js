660269dd4936a02d70cd66569d1a88e327848abf
Zach Gentner <zach.gentner@gmail.com>
2023-09-04 11:25:00 -0400

    edit btn

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ form.addEventListener("submit", (event) => {
 
 list.addEventListener("click", (event) => {
     removeTask(event);
+    editTask(event);
 });
 
 search.addEventListener("input", (event) => {
@@ -25,14 +26,21 @@ function addTask() {
         //Create a new line item element.
         let task = document.createElement("li");
 
-        //Create and append checkbox
-        let checkbox = document.createElement("button");
-        checkbox.className = "checkbox btn btn-warning";
+        //Create and append checkbox button
+        let btn = document.createElement("button");
+        btn.className = "checkbox btn btn-warning";
         let icon = document.createElement("i");
-        icon.className = "fa-solid fa-check";
+        icon.className = "checkbox fa-solid fa-check";
+        btn.appendChild(icon);
+        task.appendChild(btn);
 
-        checkbox.appendChild(icon);
-        task.appendChild(checkbox);
+        //Create and append edit button
+        btn = document.createElement("button");
+        btn.className = "edit mx-1 btn btn-secondary";
+        icon = document.createElement("i");
+        icon.className = "fa-regular fa-pen-to-square";
+        btn.appendChild(icon);
+        task.appendChild(btn);
 
         //Create and append content
         let content = document.createElement("p")
@@ -48,10 +56,37 @@ function addTask() {
 
 //REMOVE A TASK FROM THE LIST
 function removeTask(e) {
-    if(e.target.classList.contains("checkbox")) {
+    if (e.target.classList.contains("checkbox")) {
         e.target.parentElement.remove();
     } else if (e.target.parentElement.classList.contains("checkbox")) {
         e.target.parentElement.parentElement.remove();
+    }
+}
+
+//EDIT A TASK FROM THE LIST
+function editTask(e) {
+    if (e.target.classList.contains("edit")) {
+        //Remove the task from view, storing the data in a temp variable.
+        let content = e.target.parentElement.querySelector("p")
+        content.remove();
+
+        //Create a new form to replace the task text.
+        let form = document.createElement("form");
+        
+        //Create a text field for the form.
+        let field = document.createElement("input");
+        field.appendChild(document.createElement("input"));
+        field.className = ("edit m-1")
+        field.setAttribute("type", "text");
+        field.setAttribute("placeholder", content.innerText);
+
+        form.appendChild(field);
+
+        //Add the new text field as a child of the list item.
+        e.target.parentElement.appendChild(form);
+
+    } else if (e.target.parentElement.classList.contains("edit")) {
+        e.target.parentElement.parentElement.querySelector("p").remove();
     }
 }
 
